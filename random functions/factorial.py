f409c0f630e108055054f50c7fd36abec1e8bbb5
zhillyz <46723600+zhillyz@users.noreply.github.com>
2023-03-05 14:11:32 +0000

    Delete random functions directory

--- random functions/factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(value):
-    """gives the factorial of the value"""
-    fac = 1
-    for i in range(1,value+1):
-        fac *= i
-    print(fac)
-    
-factorial(4)
-
--- random functions/timing.py
+++
@@ -1,9 +0,0 @@
-import time
-def timed_func(func_to_time):
-    def timed(*args, **kwargs):
-        start = time.perf_counter()
-        res = func_to_time(*args, **kwargs)
-        print(time.perf_counter() - start)
-        return res
-    return timed
-    #blah blah blah test
